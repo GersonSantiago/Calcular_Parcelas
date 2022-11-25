708212d38a86c537eb96fe2dc040cdf10aea51eb
Gerson Cardoso Santiago <gerson_santiago_@hotmail.com>
2022-11-25 12:15:48 -0300

    Adicionando Serviços

---
+++ CalculadoraDeParcelas/Services/ServicoDoContrato.cs
@@ -0,0 +1,31 @@
+﻿using CalculadoraDeParcelas.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraDeParcelas.Services
+{
+    internal class ServicoDoContrato
+    {
+        private IServicoDePagamentoOnline _servicoDePagamentoOnline;
+
+        public ServicoDoContrato(IServicoDePagamentoOnline servicoDePagamentoOnline)
+        {
+            _servicoDePagamentoOnline = servicoDePagamentoOnline;
+        }
+
+        public void ProcessarContrato(Contrato contrato, int meses)
+        {
+            double ValorInicial = contrato.ValorTotal / meses;
+            for (int i = 1; i <= meses; i++)
+            {
+                DateTime data = contrato.Data.AddMonths(i);
+                double valorAtualizado = ValorInicial + _servicoDePagamentoOnline.TaxaDeJuros(ValorInicial, meses);
+                double valor = valorAtualizado + _servicoDePagamentoOnline.TaxaDePagamento(valorAtualizado);
+                contrato.AddParcelas(new Parcela(data, valor));
+            }
+        }
+    }
+}
---
+++ CalculadoraDeParcelas/Services/ServicoPaypal.cs
@@ -0,0 +1,8 @@
+﻿namespace CalculadoraDeParcelas.Services
+{
+    internal class ServicoPaypal
+    {
+        private const double PorcentageDePagamento = 0.02;
+        private const double PorcentagemDeJurosMensais = 0.01;
+    }
+}
