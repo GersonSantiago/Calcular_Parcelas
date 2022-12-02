f5488cec4a73360640cedfc4acd2466681272b89
Gerson Cardoso Santiago <gerson_santiago_@hotmail.com>
2022-12-02 07:09:10 -0300

    Finalizando Serviços

--- CalculadoraDeParcelas/Services/ServicoPaypal.cs
+++ CalculadoraDeParcelas/Services/ServicoPaypal.cs
@@ -1,8 +1,18 @@
 ﻿namespace CalculadoraDeParcelas.Services
 {
-    internal class ServicoPaypal
+    internal class ServicoPaypal : IServicoDePagamentoOnline
     {
         private const double PorcentageDePagamento = 0.02;
         private const double PorcentagemDeJurosMensais = 0.01;
+
+        public double TaxaDeJuros(double valor, int meses)
+        {
+            return valor * PorcentagemDeJurosMensais * meses;
+        }
+
+        public double TaxaDePagamento(double valor)
+        {
+            return valor * PorcentageDePagamento;
+        }
     }
 }
